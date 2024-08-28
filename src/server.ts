79da691aff65ce1a0a1375dceab4f4b7e132d3f1
Gustavo Gomes <gustxvogomxs@gmail.com>
2024-08-27 22:30:46 -0300

    adicao do dotenv

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,20 @@
+import dotenv from 'dotenv';
 import app from './app';
 import routes from './routes/routes';
 
-const server = async () => {
+dotenv.config();
+
+async function server() {
   try {
     // Carregar as rotas antes de iniciar o servidor
     await routes(app);
 
-    await app.listen({ 
-      port: 3000, 
-      host: '0.0.0.0'
+    const host: string = process.env.HOST || 'localhost';
+    const port: number = parseInt(process.env.PORT as string, 10) || 3000;
+
+    await app.listen({
+      host: host,
+      port: port
     });
 
     console.log("servidor rodando em http://localhost:3000");
@@ -16,6 +22,6 @@ const server = async () => {
     app.log.error(err);
     process.exit(1);
   }
-};
+}
 
 server();
